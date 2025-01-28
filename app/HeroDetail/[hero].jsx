f1cb1987521760c9d1b5da4995c9c5de8b654633
Manan Batra <mananbatra162004@gmail.com>
2025-01-28 17:36:29 +0530

    Added Ability Data in firebase

--- app/HeroDetail/[hero].jsx
+++ app/HeroDetail/[hero].jsx
@@ -1,23 +1,28 @@
-import { View, Text, Image, ImageBackground, ScrollView, TouchableOpacity, } from 'react-native';
-import React from 'react';
+import { View, Text, Image, ImageBackground, ScrollView, TouchableOpacity,ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { useLocalSearchParams } from 'expo-router';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
 import { Colors } from '../../constants/Colors';
 
 const renderShortcut = (shortcut) => {
   if (shortcut === 'PASSIVE') {
     return (
-        <Text style={{ fontFamily: 'Montserrat-SemiBold', fontSize: 13 }}>PASSIVE</Text>
+      <Text style={{ fontFamily: 'Montserrat-SemiBold', fontSize: 13 }}>PASSIVE</Text>
     );
   } else if (typeof shortcut === 'string' && shortcut.includes('.')) {
     return <Image source={{ uri: shortcut }} style={{ width: 24, height: 24 }} />;
   } else if (typeof shortcut === 'number') {
-    return <Image source={shortcut} style={{ width: 24, height: 24 }} />;
+    return <Image source={shortcut} />;
   } else {
     return <Text style={{ fontFamily: 'Montserrat-SemiBold', fontSize: 16 }}>{shortcut}</Text>;
   }
 };
 
 const AbilitiesSection = ({ sectionTitle, data }) => {
+  if (!data || data.length === 0) {
+    return null;
+  }
+
   return (
     <View style={{ width: '90%', zIndex: 2, backgroundColor: Colors.ABILITIESBG }}>
       <Text
@@ -33,7 +38,7 @@ const AbilitiesSection = ({ sectionTitle, data }) => {
       </Text>
 
       {data.map((item, index) => (
-        <TouchableOpacity key={index} >
+        <TouchableOpacity key={index}>
           <View
             style={{
               display: 'flex',
@@ -45,19 +50,24 @@ const AbilitiesSection = ({ sectionTitle, data }) => {
             }}
           >
             <View style={{ width: '15%', alignItems: 'center' }}>
-              {renderShortcut(item.shortcut)}
+              {renderShortcut(item.Key)}
             </View>
 
             <View style={{ width: 1, height: '80%', backgroundColor: Colors.ABILITIES, marginHorizontal: 10 }} />
 
             <View style={{ width: '40%', alignItems: 'center' }}>
-              <Image source={item.icon} />
+              <Image source={{ uri: item.Image }} style={{
+                width: 200,
+                height: 50
+              }}
+                resizeMode='contain'
+              />
             </View>
 
             <View style={{ width: 1, height: '80%', backgroundColor: Colors.ABILITIES, marginHorizontal: 10 }} />
 
             <View style={{ width: '35%', alignItems: 'center' }}>
-              <Text style={{ fontFamily: 'Montserrat-SemiBold', textAlign: 'center' }}>{item.name}</Text>
+              <Text style={{ fontFamily: 'Montserrat-SemiBold', textAlign: 'center' }}>{item.Attack_Name}</Text>
             </View>
           </View>
         </TouchableOpacity>
@@ -66,96 +76,87 @@ const AbilitiesSection = ({ sectionTitle, data }) => {
   );
 };
 
-
 const HeroDetail = () => {
   const { hero } = useLocalSearchParams();
+  const [heroDetails, setHeroDetails] = useState(null);
+  const [loading, setLoading] = useState(true);  
+  const [noData, setNoData] = useState(false); 
 
-  const normalData = [
-    {
-      shortcut: require('../Demoassets/Left_Mouse.png'),
-      icon: require('../Demoassets/NATTACK.png'),
-      name: 'QUANTUM MAGIC',
-      url: 'https://example.com/normal1',
-    },
-  ];
+  useEffect(() => {
+    const fetchHeroDetails = async () => {
+      const db = getFirestore();
+      const abilitiesRef = collection(db, 'Abilities');
+      const q = query(abilitiesRef, where('name', '==', hero));
 
-  const abilitiesData = [
-    {
-      shortcut: 'Q', 
-      icon: require('../Demoassets/QATTACK.png'),
-      name: 'KARMIC REVIVAL',
-      url: 'https://example.com/ability1',
-    },
-    {
-      shortcut: 'SHIFT', 
-      icon: require('../Demoassets/SHIFTATTACK.png'),
-      name: 'SOUL BOND',
-      url: 'https://example.com/ability2',
-    },
-    {
-      shortcut: 'E',
-      icon: require('../Demoassets/EATTACK.png'),
-      name: 'AVATAR LIFE STREAM',
-      url: 'https://example.com/ability3',
-    },
-    {
-      shortcut: require('../Demoassets/Right_Mouse.png'), 
-      icon: require('../Demoassets/RIGHTMATTACK.png'),
-      name: 'COSMIC CLUSTER',
-      url: 'https://example.com/ability4',
-    },
-    {
-      shortcut: 'PASSIVE', 
-      icon: require('../Demoassets/PASSIVE.png'),
-      name: 'REGENERATIVE COCOON',
-      url: 'https://example.com/ability5',
-    },
-  ];
+      try {
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const docData = querySnapshot.docs[0].data();
+          setHeroDetails(docData);
+          setNoData(false); 
+        } else {
+          setNoData(true); 
+        }
+      } catch (error) {
+        console.error("Error getting documents: ", error);
+        setNoData(true); 
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const teamUpData = [
-    {
-      shortcut: 'PASSIVE', 
-      icon: require('../Demoassets/TEAMUP.png'),
-      name: 'SOUL PERSEVERANCE',
-      url: 'https://example.com/team1',
-    },
-  ];
+    fetchHeroDetails();
+  }, [hero]);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color={Colors.ABILITIESBG} />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (noData) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>No Data Found</Text>
+      </View>
+    );
+  }
+
+  const { Normal_Attack, ABILITIES, 'TEAM-UP ABILITIES': TEAM_UP_ABILITIES } = heroDetails;
+
 
   return (
     <View style={{ width: '100%', height: '100%', display: 'flex' }}>
       <View style={{ display: 'flex', width: '100%', height: '50%', position: 'relative' }}>
         <ImageBackground
-          source={require('../../assets/images/Home.png')}
+          source={{ uri: heroDetails.Character_Bg }}
           style={{ width: '100%', height: '100%', zIndex: 0 }}
         >
           <View style={{ marginTop: '20%', display: 'flex', gap: 4, width: '50%', position: 'absolute' }}>
             <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-end', gap: 12, marginBottom: 24, paddingLeft: 20 }}>
-              <Image source={require('../../assets/images/Strategist.png')} />
-              <Text style={{ fontSize: 24, fontFamily: 'Montserrat-ExtraBoldItalic', textAlign: 'center' }}>STRATEGIST</Text>
+              <Image source={{ uri: heroDetails.Character }} />
+              <Text style={{ fontSize: 24, fontFamily: 'Montserrat-ExtraBoldItalic', textAlign: 'center' }}>
+                {heroDetails.type}
+              </Text>
             </View>
             <Text style={{ fontSize: 30, fontFamily: 'Montserrat-ExtraBoldItalic', lineHeight: 32, zIndex: 2, paddingLeft: 20 }}>
-              ADAM
-            </Text>
-            <Text style={{ fontSize: 30, fontFamily: 'Montserrat-ExtraBoldItalic', textAlign: 'right', lineHeight: 32, zIndex: 2, paddingLeft: 20 }}>
-              WARLOCK
+              {heroDetails.name}
             </Text>
           </View>
           <ImageBackground
-            source={require('../Demoassets/Adam_Warlock_Bg.png')}
+            source={{ uri: heroDetails.Character_Bg }}
             style={{ width: '100%', height: '100%', zIndex: 1, display: 'flex', alignItems: 'flex-end' }}
           >
             <Image
-              source={require('../Demoassets/Adam_Warlock.png')}
+              source={{ uri: heroDetails.Character }}
               resizeMode="cover"
               style={{ width: 230, height: '95%', marginRight: 30, zIndex: 1 }}
             />
           </ImageBackground>
         </ImageBackground>
-        <Image
-          source={require('../Demoassets/Bg_Component1.png')}
-          style={{ width: '100%', height: 60, position: 'absolute', bottom: -10 }}
-          resizeMode="contain"
-        />
       </View>
       <ImageBackground
         source={require('../Demoassets/Abilities_Bg.jpg')}
@@ -178,9 +179,9 @@ const HeroDetail = () => {
 
           <ScrollView>
             <View style={{ width: '100%', display: 'flex', alignItems: 'center', gap: 8 }}>
-              <AbilitiesSection sectionTitle="NORMAL ATTACK" data={normalData} />
-              <AbilitiesSection sectionTitle="ABILITIES" data={abilitiesData} />
-              <AbilitiesSection sectionTitle="TEAM-UP ABILITIES" data={teamUpData} />
+              <AbilitiesSection sectionTitle="NORMAL ATTACK" data={Normal_Attack} />
+              <AbilitiesSection sectionTitle="ABILITIES" data={ABILITIES} />
+              <AbilitiesSection sectionTitle="TEAM-UP ABILITIES" data={TEAM_UP_ABILITIES} />
             </View>
           </ScrollView>
         </View>
